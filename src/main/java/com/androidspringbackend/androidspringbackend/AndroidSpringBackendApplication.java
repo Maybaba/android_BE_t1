902c026c7656788cdf083de6b2a402e7cf50d7ba
Maybaba <sinyunjong@gmail.com>
2024-09-24 16:50:08 +0900

    # first init

---
+++ src/main/java/com/androidspringbackend/androidspringbackend/AndroidSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.androidspringbackend.androidspringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndroidSpringBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndroidSpringBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/androidspringbackend/androidspringbackend/AndroidSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.androidspringbackend.androidspringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndroidSpringBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
